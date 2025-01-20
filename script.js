e89d44bafd4b588bd69c0afdd3aa5088f8a2d895
ayushagarwal2502 <159728868+ayushagarwal2502@users.noreply.github.com>
2025-01-20 18:25:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
 //complete this code
 class Person {
-	constructor(name,age){
-		this.name=name;
-		this.age=age;
+	constructor(name, age) {
+		this._name = name;
+		this._age = age;
 	}
-	 name(){
-		return name;
+
+	get name() {
+		return this._name;
 	}
-	age(age){
-		this.age=age;
+
+	set age(age) {
+		this._age = age;
 	}
 }
 
 class Student extends Person {
-	constructor(name,age){
-		super(name,age)
+	constructor(name, age) {
+		super(name, age);
 	}
-	
-	study(){
 
-		console.log(this.name+" is studying");
+	study() {
+		console.log(this.name + " is studying");
 	}
 }
 
 class Teacher extends Person {
-
- constructor(name,age){
-		super(name,age)
+	constructor(name, age) {
+		super(name, age);
 	}
-	teach(){
-	  
-		console.log(this.name+" is teaching");
-	}	
+
+	teach() {
+		console.log(this.name + " is teaching");
+	}
 }
+
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
