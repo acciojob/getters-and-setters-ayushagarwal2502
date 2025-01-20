4239e2c562fe265c88466bbfb1c66828818dd98f
ayushagarwal2502 <159728868+ayushagarwal2502@users.noreply.github.com>
2025-01-20 18:21:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.name=name;
+		this.age=age;
+	}
+	 name(){
+		return name;
+	}
+	age(age){
+		this.age=age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	constructor(name,age){
+		super(name,age)
+	}
+	
+	study(){
 
-class Teacher extends Person {}
+		console.log(this.name+" is studying");
+	}
+}
 
+class Teacher extends Person {
+
+ constructor(name,age){
+		super(name,age)
+	}
+	teach(){
+	  
+		console.log(this.name+" is teaching");
+	}	
+}
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
